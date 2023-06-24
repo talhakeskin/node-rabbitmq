8499ffdcf297ada2630004e830c0b97f67e32e46
Talha Keskin <talhakskn@gmail.com>
2023-06-24 17:34:33 +0300

    added express

--- publisher/index.js
+++ publisher/index.js
@@ -1,8 +1,13 @@
+import express from "express";
 import ampqlib from "amqplib";
 import keys from "./keys.js";
+const app = express();
 const queue = "tasks";
 const connection = await ampqlib.connect(`amqp://${keys.RABBITMQ_USER}:${keys.RABBITMQ_PASSWORD}@localhost:5672`);
 const channel1 = await connection.createChannel();
 await channel1.assertQueue(queue);
 channel1.sendToQueue(queue, Buffer.from("test message"));
 channel1.sendToQueue(queue, Buffer.from("test message2"));
+app.listen(5001, () => {
+    console.log("Publisher is up and running.");
+});
--- publisher/index.ts
+++ publisher/index.ts
@@ -1,5 +1,8 @@
+import express from "express";
 import ampqlib from "amqplib";
 import keys from "./keys.js";
+
+const app = express();
 
 const queue = "tasks";
 const connection = await ampqlib.connect(`amqp://${keys.RABBITMQ_USER}:${keys.RABBITMQ_PASSWORD}@localhost:5672`);
@@ -9,3 +12,7 @@ await channel1.assertQueue(queue);
 
 channel1.sendToQueue(queue, Buffer.from("test message"));
 channel1.sendToQueue(queue, Buffer.from("test message2"));
+
+app.listen(5001, () => {
+    console.log("Publisher is up and running.");
+});
